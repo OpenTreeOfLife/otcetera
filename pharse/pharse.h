52ed4603d04e3cbffb647f8cf0555a0021466e98
Mark T. Holder <mtholder@gmail.com>
2015-02-09 21:53:27 +0100

    mom, it's broken...

--- pharse/pharse.h
+++ pharse/pharse.h
@@ -1,6 +1,33 @@
 #if !defined PHARSE_H
 #define PHARSE_H
+#include <memory>
+#include <iostream>
 
 #include "pharse/tree.h"
+namespace pharse {
 
+template<typename T>
+std::unique_ptr<RootedTree<T> > readNextNewick(std::iostream &inp);
+
+template<typename T>
+inline unsigned int readNewickStream(std::iostream &inp, bool (*callback)(std::unique_ptr<RootedTree<T> >)) {
+	auto c = 0U;
+	for (;;) {
+		std::unique_ptr<RootedTree<T> > nt = readNextNewick<T>(inp);
+		if (nt == nullptr) {
+			return c;
+		}
+		++c;
+		auto cbr = callback(std::move(nt));
+		if (!cbr) {
+			return c;
+		}
+	}
+}
+
+
+
+
+
+}
 #endif
