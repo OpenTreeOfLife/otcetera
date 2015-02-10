814567d4e18f95dc3c3c2fda28a0a9c1c31b1b69
Mark T. Holder <mtholder@gmail.com>
2015-02-10 22:38:03 +0100

    gcc reconf

---
+++ reconf-gcc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if test -z $RAPID_JSON_INC
+then
+    echo RAPID_JSON_INC must be in your env
+    exit 1
+fi
+CPPFLAGS="-I$RAPID_JSON_INC" CXX=/usr/local/bin/g++ CC=/usr/local/bin/gcc \
+    CXXFLAGS="-Waddress -Warray-bounds -Wc++11-compat -Wchar-subscripts -Wcomment -Wformat -Wmain -Wmaybe-uninitialized -Wmissing-braces -Wnonnull -Wopenmp-simd -Wparentheses -Wreorder -Wreturn-type -Wsequence-point -Wsign-compare -Wstrict-aliasing -Wstrict-overflow -Wswitch -Wtrigraphs -Wuninitialized -Wno-unknown-pragmas -Wunused-function -Wunused-label -Wunused-value -Wunused-variable -Wvolatile-register-var -Werror -Wno-pragmas -pedantic -g -O0 -std=c++11" \
+    ../configure --prefix=$PWD/installed
+
+
