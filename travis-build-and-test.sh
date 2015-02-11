fdfd43fb85c38cfb54919822265e56d020bc85f4
Mark T. Holder <mtholder@gmail.com>
2015-02-11 10:04:37 +0100

    doh

--- travis-build-and-test.sh
+++ travis-build-and-test.sh
@@ -1,4 +1,4 @@
-sh bootstraph.sh || exit
+sh bootstrap.sh || exit
 git clone https://github.com/miloyip/rapidjson.git
 mkdir buildclang
 (export RAPID_JSON_INC="$PWD/rapidjson/include" ; cd buildclang && bash ../reconf-clang.sh && make -j4 && make)
