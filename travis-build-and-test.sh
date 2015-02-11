2a0a5c9752cd0c3c8fb337d5e79092035ebae058
Mark T. Holder <mtholder@gmail.com>
2015-02-11 10:03:38 +0100

    calling our own bootstrap.sh in travis

--- travis-build-and-test.sh
+++ travis-build-and-test.sh
@@ -1,4 +1,4 @@
-autoreconf || exit
+sh bootstraph.sh || exit
 git clone https://github.com/miloyip/rapidjson.git
 mkdir buildclang
 (export RAPID_JSON_INC="$PWD/rapidjson/include" ; cd buildclang && bash ../reconf-clang.sh && make -j4 && make)
