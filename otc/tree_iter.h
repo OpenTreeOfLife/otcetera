59cf993df39b6256a50976cbe32482efa015d960
Mark T. Holder <mtholder@gmail.com>
2015-02-21 11:13:16 +0100

    refactor to take preorder iter out of ConstPreorderInternalNode namespace

--- otc/tree_iter.h
+++ otc/tree_iter.h
@@ -10,79 +10,92 @@
 
 namespace otc {
 
+template<typename T>
+using NdFilterFn = std::function<bool(const RootedTreeNode<T> &)>;
+
+template<typename T>
+class const_preorder_iterator : std::forward_iterator_tag {
+	public:
+
+	private:
+		const NdFilterFn<T> filterFn;
+		const RootedTreeNode<T> * curr;
+		bool movingDown;
+
+		void _advance() {
+			do {
+				assert(curr != nullptr);
+				if (movingDown) {
+					while (movingDown) {
+						if (curr->GetNextSib()) {
+							curr = curr->GetNextSib();
+							movingDown = false;
+						} else {
+							curr = curr->GetParent();
+							if (curr == nullptr) {
+								return;
+							}
+						}
+					}
+				} else if (curr->IsTip()) {
+					if (curr->GetNextSib()) {
+						curr = curr->GetNextSib();
+					} else {
+						movingDown = true;
+						_advance();
+						if (curr == nullptr) {
+							return;
+						}
+					}
+				} else {
+					curr = curr->GetFirstChild();
+				}
+			} while ((filterFn != nullptr && !filterFn(*curr)));
+		}
+	public:
+		const_preorder_iterator(const RootedTreeNode<T> *c)
+			:filterFn{nullptr},
+			curr(c),
+			movingDown(false) {
+		}
+		const_preorder_iterator(const RootedTreeNode<T> *c, NdFilterFn<T> f)
+			:filterFn{f},
+			curr(c),
+			movingDown(false) {
+			if (c != nullptr && filterFn && !filterFn(*curr)) {
+				_advance();
+			}
+		}
+
+		bool operator==(const const_preorder_iterator &other) {
+			return this->curr == other.curr;
+		}
+		bool operator!=(const const_preorder_iterator &other) {
+			return this->curr != other.curr;
+		}
+		const RootedTreeNode<T> * operator*() const {
+			return curr;
+		}
+		const_preorder_iterator & operator++() {
+			if (curr == nullptr) {
+				throw std::out_of_range("Incremented a dead const_preorder_iterator");
+			}
+			_advance();
+			return *this;
+		}
+};
+
 template<typename T, typename U>
 class ConstPreorderInternalNode {
 	public:
 	explicit ConstPreorderInternalNode(const RootedTree<T, U> &t)
 		:tree(t){
 	}
-	class iterator : std::forward_iterator_tag {
-		private:
-			std::function<bool(const RootedTreeNode<T> &)> filterFn;
-			const RootedTreeNode<T> * curr;
-			bool movingDown;
-			iterator(const RootedTreeNode<T> *c)
-				:filterFn{isInternalNode<T>},
-				curr(c),
-				movingDown(false) {
-				if (c != nullptr) {
-				}
-			}
-
-			void _advance() {
-				do {
-					assert(curr != nullptr);
-					if (movingDown) {
-						while (movingDown) {
-							if (curr->GetNextSib()) {
-								curr = curr->GetNextSib();
-								movingDown = false;
-							} else {
-								curr = curr->GetParent();
-								if (curr == nullptr) {
-									return;
-								}
-							}
-						}
-					} else if (curr->IsTip()) {
-						if (curr->GetNextSib()) {
-							curr = curr->GetNextSib();
-						} else {
-							movingDown = true;
-							_advance();
-							if (curr == nullptr) {
-								return;
-							}
-						}
-					} else {
-						curr = curr->GetFirstChild();
-					}
-				} while (!filterFn(*curr));
-			}
-			friend class ConstPreorderInternalNode<T, U>;
-		public:
-			bool operator==(const iterator &other) {
-				return this->curr == other.curr;
-			}
-			bool operator!=(const iterator &other) {
-				return this->curr != other.curr;
-			}
-			const RootedTreeNode<T> * operator*() const {
-				return curr;
-			}
-			iterator & operator++() {
-				if (curr == nullptr) {
-					throw std::out_of_range("Incremented a dead PreorderInternalNode::iterator");
-				}
-				_advance();
-				return *this;
-			}
-	};
-	iterator begin() const {
-		return std::move(iterator{tree.GetRoot()});
+	const_preorder_iterator<T> begin() const {
+		return std::move(const_preorder_iterator<T>{tree.GetRoot(), isInternalNode<T>});
 	}
-	iterator end() const {
-		return iterator{nullptr};
+	const_preorder_iterator<T> end() const {
+		return const_preorder_iterator<T>{nullptr};
 	}
 	private:
 		const RootedTree<T, U> & tree;
