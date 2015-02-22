e2e9f734d0805b3918893dd6d49c3e5f4f038de6
Mark T. Holder <mtholder@gmail.com>
2015-02-22 21:10:41 +0100

    configure invocation for clang with O3

---
+++ reconf-clang-release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if test -z $RAPID_JSON_INC
+then
+    echo RAPID_JSON_INC must be in your env
+    exit 1
+fi
+set -x
+CPPFLAGS="-I$RAPID_JSON_INC" CXX=$(which clang++) CC=$(which clang) \
+    CXXFLAGS="-Wno-c++98-compat -Weverything -Wpadded -pedantic -O3 -std=c++1y -cxx-isystem=/usr/lib/gcc/x86_64-linux-gnu/4.9" \
+    ../configure --prefix=$PWD/installed
+
+
