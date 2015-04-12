cce4c28edb2825eb250c95a79397ca8313602ce8
Mark T. Holder <mtholder@gmail.com>
2015-04-12 21:20:19 +0200

    simple scripts for creating intersection or difference of sets of IDs

---
+++ test/xor_of_lists.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+try:
+    f, s = sys.argv[1:]
+except:
+    sys.exit('Expecting two files: the lines of each will be compared.')
+fc = set([i.strip() for i in open(f, 'rU').readlines()])
+sc = set([i.strip() for i in open(s, 'rU').readlines()])
+i = list(fc.symmetric_difference(sc))
+i.sort()
+sys.stdout.write('\n'.join(i))
+sys.stdout.write('\n')
