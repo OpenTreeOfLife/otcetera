b3c4b37fa71d159706f9cd1b8d8cfad298244b51
Mark T. Holder <mtholder@gmail.com>
2015-05-01 14:09:36 +0200

    allowing old-style casts

--- reconf-clang-release.sh
+++ reconf-clang-release.sh
@@ -5,8 +5,8 @@ then
     exit 1
 fi
 set -x
-CPPFLAGS="-I$RAPID_JSON_INC" CXX=$(which clang++) CC=$(which clang) \
-    CXXFLAGS="-Wno-c++98-compat -Weverything -Wpadded -pedantic -O3 -std=c++1y -cxx-isystem=/usr/lib/gcc/x86_64-linux-gnu/4.9" \
+CPPFLAGS="-I$RAPID_JSON_INC" CXX=$(which clang++-3.5) CC=$(which clang-3.5) \
+    CXXFLAGS="-Wno-c++98-compat -Wno-old-style-cast -Weverything -Wpadded -pedantic -O3 -std=c++1y -cxx-isystem=/usr/lib/gcc/x86_64-linux-gnu/4.9" \
     ../configure --prefix=$PWD/installed
 
 
