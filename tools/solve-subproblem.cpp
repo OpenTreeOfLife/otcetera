125ccc73f7cf994821ddb3f826368df65613a3ba
Benjamin Redelings <benjamin.redelings@gmail.com>
2015-09-30 22:55:30 -0700

    Clean up comments and debugging messages.

--- tools/solve-subproblem.cpp
+++ tools/solve-subproblem.cpp
@@ -57,7 +57,7 @@ std::ostream& operator<<(std::ostream& o, const RSplit& s)
   return o;
 }
 
-/// Merge components c1 and c2
+/// Merge components c1 and c2 and return the component name that survived
 long merge_components(long c1, long c2, map<long,long>& component, map<long,list<long>>& elements)
 {
   if (elements[c2].size() > elements[c1].size())
@@ -87,6 +87,7 @@ bool empty_intersection(const set<long>& x, const set<long>& y)
   return true;
 }
 
+/// Construct a tree with all the splits mentioned, and return a null pointer if this is not possible
 unique_ptr<Tree_t> BUILD(const std::set<long>& tips, const vector<RSplit>& splits)
 {
   std::unique_ptr<Tree_t> tree(new Tree_t());
@@ -130,16 +131,12 @@ unique_ptr<Tree_t> BUILD(const std::set<long>& tips, const vector<RSplit>& split
     }
   }
 
-  // 4. If we can't subdivide the leaves in any way, then the splits are not consistent.
+  // 4. If we can't subdivide the leaves in any way, then the splits are not consistent, so return failure
   long first = *tips.begin();
   if (elements[component[first]].size() == tips.size())
-  {
-    std::cerr<<"Failure: 1 component!\n";
     return {};
-  }
 
-  // 5. Create the sets describing each component
-  std::cerr<<"Components:\n";
+  // 5. Create the set of tips in each connected component 
   map<long,set<long>> subtips;
   for(long c: tips)
   {
@@ -148,7 +145,6 @@ unique_ptr<Tree_t> BUILD(const std::set<long>& tips, const vector<RSplit>& split
     set<long>& s = subtips[c];
     for(long l: elements[c])
       s.insert(l);
-    std::cerr<<"   "<<s<<"\n";
   }
 
   // 6. Determine the splits that are not satisfied yet and go into each component
@@ -158,7 +154,7 @@ unique_ptr<Tree_t> BUILD(const std::set<long>& tips, const vector<RSplit>& split
     long first = *split.in.begin();
     long c = component[first];
 
-    // if none of the exclude group are in the component, the the split is satisfied by the top-level partition.
+    // if none of the exclude group are in the component, then the split is satisfied by the top-level partition.
     if (empty_intersection(split.out, subtips[c])) continue;
 
     subsplits[c].push_back(split);
@@ -167,7 +163,6 @@ unique_ptr<Tree_t> BUILD(const std::set<long>& tips, const vector<RSplit>& split
   // 7. Recursively solve the sub-problems of the partition components
   for(const auto& x: subtips)
   {
-    int c = x.first;
     auto subtree = BUILD(x.second, subsplits[x.first]);
     if (not subtree) return {};
 
@@ -178,6 +173,7 @@ unique_ptr<Tree_t> BUILD(const std::set<long>& tips, const vector<RSplit>& split
 }
 
 
+/// Run the BUILD algorithm on the first n splits
 unique_ptr<Tree_t> BUILD(const std::set<long>& tips, const vector<RSplit>& splits, int n)
 {
   vector<RSplit> splits2;
@@ -186,6 +182,7 @@ unique_ptr<Tree_t> BUILD(const std::set<long>& tips, const vector<RSplit>& split
   return BUILD(tips,splits2);
 }
 
+/// What madness is being perpetrated here?
 string fixname(string name)
 {
   for(char& c: name)
@@ -194,89 +191,85 @@ string fixname(string name)
   return name;
 }
 
+/// Copy node names from taxonomy to tree based on ott ids, and copy the root name also
 void add_names(unique_ptr<Tree_t>& tree, const unique_ptr<Tree_t>& taxonomy)
 {
   map<long,string> names;
   
-  // 8. Add writable names to the tree
+  // 1. Determine the names for each ottid
   for(auto nd: iter_post(*taxonomy))
     if (nd->isTip())
-      names[nd->getOttId()] = nd->getName();
+      names[nd->getOttId()] = fixname(nd->getName());
   
-  // 8. Add writable names to the tree
+  // 2. Set the names of nodes based on their ottid
   for(auto nd: iter_post(*tree))
     if (nd->hasOttId())
-    {
-      string name = names[nd->getOttId()];
-      nd->setName(fixname(name));
-    }
+      nd->setName( names[nd->getOttId()] );
   
-  tree->getRoot()->setName(fixname(taxonomy->getRoot()->getName()));
+  // 3. Name the root node too
+  string rootname = taxonomy->getRoot()->getName();
+  tree->getRoot()->setName(fixname(rootname));
 }
 
+/// Get the list of splits, and add them one at a time if they are consistent with previous splits
 unique_ptr<Tree_t> combine(const vector<unique_ptr<Tree_t>>& trees)
 {
   // Standardize names to 0..n-1 for this subproblem
   const auto& taxonomy = trees.back();
   auto all_leaves = taxonomy->getRoot()->getData().desIds;
-  std::cerr<<all_leaves<<std::endl;
   
+  // 1. Find splits in order of input trees
   vector<RSplit> splits;
   for(const auto& tree: trees)
   {
-    std::cerr<<"Tree!\n";
     auto root = tree->getRoot();
     const auto leafTaxa = root->getData().desIds;
+
+    for(const auto& leaf: set_difference_as_set(leafTaxa, all_leaves))
+      throw OTCError()<<"OTT Id "<<leaf<<" not in taxonomy!";
+      
     for(auto nd: iter_post_const(*tree))
     {
       const auto& descendants = nd->getData().desIds;
       RSplit split{descendants, leafTaxa};
       if (split.in.size()>1 and split.out.size())
-      {
       	splits.push_back(split);
-	std::cerr<<split.in<<" | "<<split.out<<"\n";
-      }
     }
-    BUILD(all_leaves, splits);
   }
 
-  for(int n=2;n<=splits.size();)
+  // 2. Add splits sequentially if they are consistent with previous splits.
+  vector<RSplit> consistent;
+  for(const auto& split: splits)
   {
-    unique_ptr<Tree_t> result = BUILD(all_leaves, splits, n);
+    consistent.push_back(split);
+    auto result = BUILD(all_leaves, consistent);
     if (not result)
-      splits.erase(splits.begin()+(n-1));
+    {
+      consistent.pop_back();
+      LOG(DEBUG)<<"Reject: "<<split<<"\n";
+    }
     else
-      n++;
+      LOG(DEBUG)<<"Keep:   "<<split<<"\n";
   }
 
-  auto tree = BUILD(all_leaves, splits, splits.size());
+  // 3. Construct final tree and add names
+  auto tree = BUILD(all_leaves, consistent);
   add_names(tree, taxonomy);
   return tree;
 }
 
 int main(int argc, char *argv[]) {
-    OTCLI otCLI("otc-subproblem-stats",
-                "takes at series of tree files. Each is treated as a subproblem.\n" \
-                "For each subproblem there is tab-separated report for:\n" \
-                "    Subproblem name\n" \
-                "    InSp = # of informative (nontrivial) splits\n" \
-                "    LSS = size of the leaf label set\n" \
-                "    ILSS = size of the set of labels included in at least one \"ingroup\"\n" \
-                "    NT = The number of trees.\n" \
-                "    TreeSummaryName = tree index or summary name\n" \
-                "where the summary name can be Phylo-only or Total. \n" \
-                "  \"Total\" summarizes info all trees in the file (including the taxonomy).\n" \
-                "  \"Phylo-only\" former summarizes all of the phylogenetic inputs",
-                "taxonomy.tre supertree/step_7_scratch/export-sub-temp/ott1000236.tre supertree/step_7_scratch/export-sub-temp/ott1000250.tre");
+    OTCLI otCLI("otc-solve-subproblem",
+                "takes at series of tree files. Each is treated as a subproblem.\n",
+		"subproblem.tre");
 
     vector<unique_ptr<Tree_t>> trees;
     auto get = [&trees](OTCLI &, unique_ptr<Tree_t> nt) {trees.push_back(std::move(nt)); return true;};
 
-    // Want splits for each tip.
-    // Not sure these trees are aligned to the taxonomy
-    // They should have only terminals in desIds.
+    // I don't think multiple subproblem files are essentially concatenated.
+    // Is it possible to read a single subproblem from cin?
     treeProcessingMain<Tree_t>(otCLI, argc, argv, get, nullptr, 1);
-    
+
     auto tree = combine(trees);
     
     tree->writeAsNewick(std::cout, true);
