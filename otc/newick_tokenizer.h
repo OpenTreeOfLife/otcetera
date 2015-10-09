58c66ed42b87a7430c6eae13f82356707d45afe7
Benjamin Redelings <benjamin.redelings@gmail.com>
2015-10-09 18:37:52 -0400

    Check for the presence of some kind of separator before "ott".

--- otc/newick_tokenizer.h
+++ otc/newick_tokenizer.h
@@ -356,7 +356,9 @@ inline long ottIDFromName(const std::string & n) {
             break;
         }
     }
-    if (currInd < 3 or strncmp(c+currInd-3,"ott",3) != 0) return -2;
+    if (currInd < 3) return 2;
+    if (currInd >= 3 and strncmp(c+currInd-3,"ott",3) != 0) return -2;
+    if (currInd > 3 and strchr(" \t",c[currInd-4]) == 0) return -2;
     long conv = -2;
     auto r = char_ptr_to_long(c + currInd, &conv);
     assert(r);
