b3d82c8259e3326d1597dc221d0b210d3907f187
Benjamin Redelings <benjamin.redelings@gmail.com>
2016-03-28 17:43:10 -0400

    Add some additional output/query options.

--- tools/taxonomy-parser.cpp
+++ tools/taxonomy-parser.cpp
@@ -126,6 +126,11 @@ variables_map parse_cmd_line(int argc,char* argv[])
 
     options_description output("Output options");
     output.add_options()
+        ("find,S",value<string>(),"Show taxa whose names match regex <arg>")
+        ("degree,D",value<long>(),"Show out the degree of node <arg>")
+        ("children,C",value<long>(),"Show the children of node <arg>")
+        ("parent,P",value<long>(),"Show the parent taxon of node <arg>")
+        ("high-degree-nodes",value<int>(),"Show the top <arg> high-degree nodes")
         ("write-tree,T","Write out the result as a tree")
         ("write-taxonomy",value<string>(),"Write out the result as a taxonomy to directory 'arg'")
         ("name,N", value<long>(), "Return name of the given ID")
@@ -214,6 +219,11 @@ void report_lost_taxa(const Taxonomy& taxonomy, const string& filename)
             std::cout<<"depth="<<rec->depth<<"   id="<<rec->id<<"   uniqname='"<<rec->uniqname<<"'\n";
 }
 
+void show_rec(const taxonomy_record& rec)
+{
+    std::cout<<rec.id<<"   '"<<rec.uniqname<<"'   '"<<rec.rank<<"'   depth = "<<rec.depth<<"   out-degree = "<<rec.out_degree<<"\n";
+}
+
 int main(int argc, char* argv[])
 {
     std::ios::sync_with_stdio(false);
@@ -241,6 +251,54 @@ int main(int argc, char* argv[])
 
         Taxonomy taxonomy(taxonomy_dir, cleaning_flags, keep_root);
 
+        if (args.count("find"))
+        {
+            string s = args["find"].as<string>();
+            std::regex e(s);
+            for(const auto& rec: taxonomy)
+            {
+                std::cmatch m;
+                if (std::regex_match(rec.name.data(), rec.name.data()+rec.name.size(), m, e))
+                    show_rec(rec);
+            }
+            exit(0);
+        }
+        else if (args.count("degree"))
+        {
+            long id = args["degree"].as<long>();
+            std::cout<<"degree = "<<taxonomy[taxonomy.index.at(id)].out_degree<<std::endl;
+            exit(0);
+        }
+        else if (args.count("children"))
+        {
+            long id = args["children"].as<long>();
+
+            for(const auto& rec: taxonomy)
+            {
+                long parent_id = taxonomy[rec.parent_index].id;
+                if (parent_id == id)
+                    show_rec(rec);
+            }
+            exit(0);
+        }
+        else if (args.count("parent"))
+        {
+            long id = args["parent"].as<long>();
+            auto parent_index = taxonomy[taxonomy.index.at(id)].parent_index;
+            std::cout<<"parent = "<<taxonomy[parent_index].id<<std::endl;
+            exit(0);
+        }
+        else if (args.count("high-degree-nodes"))
+        {
+            int n = args["high-degree-nodes"].as<int>();
+            vector<int> index(taxonomy.size());
+            for(int i=0;i<index.size();i++)
+                index[i] = i;
+            std::sort(index.begin(), index.end(), [&taxonomy](int i, int j){return taxonomy[i].out_degree > taxonomy[j].out_degree;});
+            for(int i=0;i<n;i++)
+                show_rec(taxonomy[index[i]]);
+            exit(0);
+        }
         if (args.count("write-tree"))
         {
             auto nodeNamer = [](const auto& record){return string(record.name)+"_ott"+std::to_string(record.id);};
