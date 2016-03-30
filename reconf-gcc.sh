01d4e0fbd511a0484b08ac66356ed62355f82de7
Mark T. Holder <mtholder@gmail.com>
2016-03-30 12:01:39 -0500

    clang to gcc for travis build

--- reconf-gcc.sh
+++ reconf-gcc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CXX=/usr/bin/g++ CC=/usr/bin/gcc \
+CXX=$(which g++) CC=$(which gcc) \
     CXXFLAGS="-Waddress -Warray-bounds -Wc++11-compat -Wchar-subscripts -Wcomment -Wformat -Wmain -Wmaybe-uninitialized -Wmissing-braces -Wnonnull -Wopenmp-simd -Wparentheses -Wreorder -Wreturn-type -Wsequence-point -Wsign-compare -Wstrict-aliasing -Wstrict-overflow -Wswitch -Wtrigraphs -Wuninitialized -Wno-unknown-pragmas -Wunused-function -Wunused-label -Wunused-value -Wunused-variable -Wvolatile-register-var -Wno-pragmas -pedantic -g -O0 -std=c++14" \
     ../configure --prefix=$PWD/installed
 
--- travis-build-and-test.sh
+++ travis-build-and-test.sh
@@ -1,3 +1,3 @@
 sh bootstrap.sh || exit
-mkdir buildclang
-(cd buildclang && bash ../reconf-clang.sh && make -j4 && make)
+mkdir buildgcc
+(cd buildgcc && bash ../reconf-gcc.sh && make -j4 && make)
