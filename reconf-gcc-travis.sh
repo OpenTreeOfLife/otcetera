f88a73b8214e8423c6eac80a0bf36037a2d872be
Mark T. Holder <mtholder@gmail.com>
2016-03-30 12:13:25 -0500

    trying to get g++-5

---
+++ reconf-gcc-travis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+CXX=$(which g++-4.9) CC=$(which gcc-4.9) \
+    CXXFLAGS="-Waddress -Warray-bounds -Wc++11-compat -Wchar-subscripts -Wcomment -Wformat -Wmain -Wmaybe-uninitialized -Wmissing-braces -Wnonnull -Wopenmp-simd -Wparentheses -Wreorder -Wreturn-type -Wsequence-point -Wsign-compare -Wstrict-aliasing -Wstrict-overflow -Wswitch -Wtrigraphs -Wuninitialized -Wno-unknown-pragmas -Wunused-function -Wunused-label -Wunused-value -Wunused-variable -Wvolatile-register-var -Wno-pragmas -pedantic -g -O0 -std=c++14" \
+    ../configure --prefix=$PWD/installed
+
+
