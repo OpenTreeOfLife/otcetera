ca5010bbfd0cdb63aea94fd8041d2bfe041ea5dd
Mark T. Holder <mtholder@gmail.com>
2016-03-30 12:50:57 -0500

    libboost-filesystem...

--- travis-build-and-test.sh
+++ travis-build-and-test.sh
@@ -1,5 +1,4 @@
 sh bootstrap.sh || exit
 mkdir buildgcc
 (cd buildgcc || exit ; \
- bash ../reconf-gcc-travis.sh || cat config.log; \
- make -j4 && make)
+ bash ../reconf-gcc-travis.sh && make -j4 && make)
