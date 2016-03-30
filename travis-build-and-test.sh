8de0522a881d256110213991031361115b939901
Mark T. Holder <mtholder@gmail.com>
2016-03-30 09:55:23 -0500

    need -y on sudo apt-get on travis. and no longer need rapidjson

--- travis-build-and-test.sh
+++ travis-build-and-test.sh
@@ -1,4 +1,3 @@
 sh bootstrap.sh || exit
-git clone https://github.com/miloyip/rapidjson.git
 mkdir buildclang
-(export RAPID_JSON_INC="$PWD/rapidjson/include" ; cd buildclang && bash ../reconf-clang.sh && make -j4 && make)
+(cd buildclang && bash ../reconf-clang.sh && make -j4 && make)
