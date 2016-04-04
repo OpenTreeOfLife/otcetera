4ab94d850748d76a50913e62bec608b8d02bdf0b
Benjamin Redelings <benjamin.redelings@gmail.com>
2016-04-04 15:08:42 -0400

    Use them in annotate-synth.

--- tools/annotate-synth.cpp
+++ tools/annotate-synth.cpp
@@ -7,6 +7,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include "json.hpp"
+#include "otc/induced_tree.h"
 
 using namespace otc;
 using json = nlohmann::json;
@@ -67,7 +68,6 @@ int depth(const Tree_t::node_type* node);
 int& depth(Tree_t::node_type* node);
 Tree_t::node_type* summary_node(const Tree_t::node_type* node);
 Tree_t::node_type*& summary_node(Tree_t::node_type* node);
-Tree_t::node_type* nmParent(Tree_t::node_type* node);
 void computeSummaryLeaves(Tree_t& tree, const map<long,Tree_t::node_type*>& summaryOttIdToNode);
 string getSourceNodeNameIfAvailable(const Tree_t::node_type* node);
 Tree_t::node_type* get_root(Tree_t::node_type* node);
@@ -92,14 +92,6 @@ inline Tree_t::node_type* summary_node(const Tree_t::node_type* node) {
 
 inline Tree_t::node_type*& summary_node(Tree_t::node_type* node) {
     return node->getData().summary_node;
-}
-
-// returns the most recent non-monotypic ancestor of node (or nullptr if there is no such node)
-inline Tree_t::node_type* nmParent(Tree_t::node_type* node) {
-    do {
-        node = node->getParent();
-    } while (node and node->isOutDegreeOneNode());
-    return node;
 }
 
 // Compute number of tips at this node or below it
@@ -149,58 +141,6 @@ const Tree_t::node_type* get_root(const Tree_t::node_type* node)
     return node;
 }
 
-// assumes `node` is marked with `bits`. Returns the parent of `node` and
-//  also marks the parent with `bits` as a side effect.
-const node_t* trace_to_parent(const node_t* node, set<const node_t*>& nodes) {
-    assert(nodes.count(node));
-    node = node->getParent(); // move to parent and insert it.
-    nodes.insert(node);
-    return node;
-}
-
-node_t* trace_to_parent(node_t* node, set<node_t*>& nodes) {
-    assert(nodes.count(node));
-    node = node->getParent(); // move to parent and insert it.
-    nodes.insert(node);
-    return node;
-}
-
-/// Walk up the tree from node1 and node2 until we find the common ancestor, putting nodes into set `nodes`.
-template <typename N>
-N* trace_find_MRCA(N* node1, N* node2, set<N*>& nodes)
-{
-    assert(node1 or node2);
-    if (not node1) {
-        assert(nodes.count(node2));
-        return node2;
-    }
-    if (not node2) {
-        assert(nodes.count(node2));
-        return node1;
-    }
-    assert(node1 and node2);
-    assert(get_root(node1) == get_root(node2));
-    assert(nodes.count(node1));
-    assert(nodes.count(node2));
-    while (depth(node1) > depth(node2)){
-        node1 = trace_to_parent(node1, nodes);
-    }
-    while (depth(node1) < depth(node2)){
-        node2 = trace_to_parent(node2, nodes);
-    }
-    assert(depth(node1) == depth(node2));
-    while (node1 != node2) {
-        assert(node1->getParent());
-        assert(node2->getParent());
-        node1 = trace_to_parent(node1, nodes);
-        node2 = trace_to_parent(node2, nodes);
-    }
-    assert(node1 == node2);
-    assert(nodes.count(node1));
-    assert(nodes.count(node2));
-    return node1;
-}
-
 template <typename N>
 vector<N*> leaf_nodes_below(N* node)
 {
@@ -232,101 +172,6 @@ auto find_induced_nodes(const vector<node_t*>& leaves)
         vnodes.push_back(nd);
     return vnodes;
 }
-
-std::unique_ptr<Tree_t> get_induced_tree(const std::vector<const node_t*>& leaves)
-{
-    // 1. Find MRCA and all nodes in the tree
-    const Tree_t::node_type* MRCA = nullptr;
-    set<const node_t*> nodes;
-    for(auto leaf: leaves)
-    {
-        nodes.insert(leaf);
-        MRCA = trace_find_MRCA(MRCA, leaf, nodes);
-    }
-
-    std::unique_ptr<Tree_t> induced_tree(new Tree_t());
-
-    // 2. Construct duplicate nodes for the induced tree, recording correspondence
-    map<const node_t*, node_t*> to_induced_tree;
-    for(auto nd: nodes)
-    {
-        auto nd2 = induced_tree->createNode(nullptr);
-
-        if (nd->hasOttId())
-            nd2->setOttId(nd->getOttId());
-
-        if (nd->getName().size())
-            nd2->setName(nd->getName());
-
-        to_induced_tree[nd] = nd2;
-    }
-
-    // 3. Link corresponding nodes to their corresponding parents
-    for(auto nd: nodes)
-    {
-        auto p = nd->getParent();
-
-        auto nd2 = to_induced_tree.find(nd)->second;
-        auto p2_it = to_induced_tree.find(p);
-
-        if (p2_it == to_induced_tree.end())
-        {
-            assert(nd == MRCA);
-        }
-        else
-        {
-            auto p2 = p2_it->second;
-            p2->addChild(nd2);
-        }
-    }
-
-    // 4. Set the root of the induced tree to node corresponding to the MRCA
-    induced_tree->_setRoot( to_induced_tree.at(MRCA) );
-    
-    return induced_tree;
-}
-
-// Get a list of leaves of tree 1 that are also in tree 2.
-vector<const Tree_t::node_type*> get_induced_leaves(const Tree_t& T1, const map<long, const Tree_t::node_type*>& nodes1,
-                                                    const Tree_t& T2, const map<long, const Tree_t::node_type*>& nodes2)
-{
-    vector<const Tree_t::node_type*> leaves;
-    
-    if (nodes2.size() < nodes1.size())
-    {
-        for(auto leaf: iter_leaf_const(T2))
-        {
-            auto id = leaf->getOttId();
-            auto it = nodes1.find(id);
-            if (it != nodes1.end())
-                leaves.push_back(it->second);
-        }
-    }
-    else
-    {
-        for(auto leaf: iter_leaf_const(T1))
-        {
-            auto id = leaf->getOttId();
-            if (nodes2.find(id) != nodes2.end())
-                leaves.push_back(leaf);
-        }
-    }
-
-    return leaves;
-}
-
-// Get the subtree of T1 connecting the leaves of T1 that are also in T2.
-std::unique_ptr<Tree_t> get_induced_tree(const Tree_t& T1, const map<long, const Tree_t::node_type*>& nodes1,
-                                         const Tree_t& T2, const map<long, const Tree_t::node_type*>& nodes2)
-{
-    auto induced_leaves = get_induced_leaves(T1, nodes1, T2, nodes2);
-
-    auto induced_tree =  get_induced_tree(induced_leaves);
-    induced_tree->setName(T1.getName());
-
-    return induced_tree;
-}
-
 
 json get_support_blob_as_array(const Map<string,string>& M)
 {
@@ -509,8 +354,8 @@ void perform_conflict_analysis(const Tree_t& tree1,
                                node_logger_t log_terminal)
 {
     // Handle non-leaf correspondence?
-    auto induced_tree1 = get_induced_tree(tree1, ottid_to_node1, tree2, ottid_to_node2);
-    auto induced_tree2 = get_induced_tree(tree2, ottid_to_node2, tree1, ottid_to_node1);
+    auto induced_tree1 = get_induced_tree<Tree_t>(tree1, ottid_to_node1, tree2, ottid_to_node2);
+    auto induced_tree2 = get_induced_tree<Tree_t>(tree2, ottid_to_node2, tree1, ottid_to_node1);
 
     computeDepth(*induced_tree1);
     compute_tips(*induced_tree1);
