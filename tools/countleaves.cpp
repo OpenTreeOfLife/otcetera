788ee87a23ede214449c1ce4b34e17d25146e138
Mark T. Holder <mtholder@gmail.com>
2016-04-12 12:39:41 -0500

    letting otc-count-tips list full names

--- tools/countleaves.cpp
+++ tools/countleaves.cpp
@@ -7,8 +7,10 @@ template<typename T>
 bool writeNumLeaves(OTCLI & , std::unique_ptr<T> tree);
 template<typename T>
 bool listTipOttIds(OTCLI & , std::unique_ptr<T> tree);
-
+template<typename T>
+bool listTipNames(OTCLI & , std::unique_ptr<T> tree);
 static bool listTips = false;
+static bool listNames = false;
 
 template<typename T>
 bool listTipOttIds(OTCLI & , T * tree) {
@@ -20,8 +22,20 @@ bool listTipOttIds(OTCLI & , T * tree) {
 }
 
 template<typename T>
+bool listTipNames(OTCLI & , T * tree) {
+    for (auto nd : iter_leaf_const(*tree)) {
+        assert(nd->hasOttId());
+        std::cout << nd->getName() << '\n';
+    }
+    return true;
+}
+
+template<typename T>
 bool writeNumLeaves(OTCLI & otCLI, std::unique_ptr<T> tree) {
     if (listTips) {
+        if (listNames) {
+            return listTipNames(otCLI, tree.get());
+        }
         return listTipOttIds(otCLI, tree.get());
     }
     auto c = 0U;
@@ -42,6 +56,12 @@ bool handleListTips(OTCLI & , const std::string &) {
     return true;
 }
 
+bool handleListNameTips(OTCLI & , const std::string &) {
+    listTips = true;
+    listNames = true;
+    return true;
+}
+
 int main(int argc, char *argv[]) {
     OTCLI otCLI("otc-count-leaves",
                  "takes a filepath to a newick file and reports the number of leaves",
@@ -50,6 +70,10 @@ int main(int argc, char *argv[]) {
     otCLI.addFlag('l',
                   "If present, list the tip OTT IDs rather than counting them",
                   handleListTips,
+                  false);
+    otCLI.addFlag('n',
+                  "If present, names rather than counting them",
+                  handleListNameTips,
                   false);
     return treeProcessingMain<Tree_t>(otCLI, argc, argv, wnl, nullptr, 1);
 }
