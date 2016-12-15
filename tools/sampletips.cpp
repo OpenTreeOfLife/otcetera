289bcfdb518503dd5a765fb25c75655b16be281c
Mark T. Holder <mtholder@gmail.com>
2016-12-15 11:34:03 -0600

    -s option of sample tips to sample named nodes more completely

--- tools/sampletips.cpp
+++ tools/sampletips.cpp
@@ -26,12 +26,22 @@ template<typename T>
 void writeTreesFromSampledTipsToEncodeStructureDecoratedTree(const T & tree, std::ostream & outStream);
 template<typename N>
 void writeSmallestIDForEachChild(const N &ndRef, std::ostream & outStream);
+template<typename T>
+void writeTreesFromSampledTipsOnePerInternalAllChildrenOneSib(const T & tree, std::ostream & outStream);
+template<typename T>
+void writeTreesFromSampledTipsNamedInternalsOneDesPerOutgoing(const T & tree, std::ostream & outStream);
 
+enum SampleMode {
+    TREE_PER_INTERNAL_ALL_CHILDREN_ONE_SIB = 0,
+    NAMED_INTERNALS_ONE_DES_PER_OUTGOING = 1
+};
+
+SampleMode gSampleMode = SampleMode::TREE_PER_INTERNAL_ALL_CHILDREN_ONE_SIB;
 
 template<typename T>
-void writeTreesFromSampledTipsToEncodeStructure(T & tree, std::ostream & outStream) {
+void writeTreesFromSampledTipsToEncodeStructure(T & tree, std::ostream & outStream, SampleMode sm) {
     calculateSmallestChild(tree);
-    writeTreesFromSampledTipsToEncodeStructureDecoratedTree(tree, outStream);
+    writeTreesFromSampledTipsToEncodeStructureDecoratedTree(tree, outStream, sm);
 }
 
 
@@ -51,7 +61,17 @@ void writeSmallestIDForEachChild(const N &ndRef, std::ostream & outStream) {
 }
 
 template<typename T>
-void writeTreesFromSampledTipsToEncodeStructureDecoratedTree(const T & tree, std::ostream & outStream) {
+void writeTreesFromSampledTipsToEncodeStructureDecoratedTree(const T & tree, std::ostream & outStream, SampleMode sm) {
+    if (sm == SampleMode::TREE_PER_INTERNAL_ALL_CHILDREN_ONE_SIB) {
+        writeTreesFromSampledTipsOnePerInternalAllChildrenOneSib(tree, outStream);
+    } else {
+        assert(sm == SampleMode::NAMED_INTERNALS_ONE_DES_PER_OUTGOING);
+        writeTreesFromSampledTipsNamedInternalsOneDesPerOutgoing(tree, outStream);
+    }
+}
+
+template<typename T>
+void writeTreesFromSampledTipsOnePerInternalAllChildrenOneSib(const T & tree, std::ostream & outStream) {
     for (auto nd: iter_post_const(tree)){
         if (nd->isTip()) {
             continue;
@@ -81,6 +101,64 @@ void writeTreesFromSampledTipsToEncodeStructureDecoratedTree(const T & tree, std
 }
 
 
+template<typename N>
+void emitNewickForSubtreeIndirectRecursion(const N * node, std::ostream & outStream);
+template<typename N>
+void emitNewickForNamedInternal(const N * node, std::ostream & outStream, bool isRootOfSubtree);
+
+template<typename N>
+void emitNewickForSubtreeIndirectRecursion(const N * node, std::ostream & outStream) {
+    outStream << '(';
+    bool first_child = true;
+    for (auto c: iter_child_const(*node)) {
+        if (first_child) {
+            first_child = false;
+        } else {
+            outStream << ',';
+        }
+        emitNewickForNamedInternal(c, outStream, false);
+    }
+    outStream << ')';
+}
+
+template<typename N>
+void emitNewickForNamedInternal(const N * node, std::ostream & outStream, bool isRootOfSubtree) {
+    if (isRootOfSubtree) {
+        emitNewickForSubtreeIndirectRecursion(node, outStream);
+        outStream << ";\n";
+    } else {
+        if (node->hasOttId()) {
+            if (node->isTip()) {
+                outStream << "ott" << node->getOttId();
+            } else {
+                outStream << "ott" << smallestChild(node);
+            }
+        } else {
+            emitNewickForSubtreeIndirectRecursion(node, outStream);
+        }
+    }
+}
+
+template<typename T>
+void writeTreesFromSampledTipsNamedInternalsOneDesPerOutgoing(const T & tree, std::ostream & outStream) {
+    for (auto nd: iter_post_const(tree)){
+        if (nd->isTip()) {
+            continue;
+        }
+        auto anc = findFirstForkingAnc(nd);
+        if (nd->hasOttId() || anc == nullptr) {
+            emitNewickForNamedInternal(nd, outStream, true);
+        }
+    }
+}
+
+
+bool handleSubproblemSampling(OTCLI &, const std::string &) {
+    gSampleMode = SampleMode::NAMED_INTERNALS_ONE_DES_PER_OUTGOING;
+    return true;
+}
+
+
 int main(int argc, char *argv[]) {
     OTCLI otCLI("otc-sample-tips",
                 "Takes a rooted phylogenetic estimate (the first tree file) and writes a series of trees that are induced\n" \
@@ -90,6 +168,10 @@ int main(int argc, char *argv[]) {
                 "This encoding is done by contains a descendant of each child of the node\n" \
                 "    and descendant of one of the node\'s siblings.",
                 "tree-to-sample.tre");
+    otCLI.addFlag('s',
+                  "Subproblem sampling. An output tree for every named input with all outgoing arcs sampled.",
+                  handleSubproblemSampling,
+                  false);
     vector<unique_ptr<Tree_t>> trees;
     auto get = [&trees](OTCLI &, unique_ptr<Tree_t> nt) {trees.push_back(std::move(nt)); return true;};
     if (argc < 2) {
@@ -103,6 +185,6 @@ int main(int argc, char *argv[]) {
         return 2;
     }
     auto & solution = *(trees.at(0));
-    writeTreesFromSampledTipsToEncodeStructure(solution, std::cout);
+    writeTreesFromSampledTipsToEncodeStructure(solution, std::cout, gSampleMode);
     return 0;
 }
