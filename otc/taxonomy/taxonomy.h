bfae1738b74ebdbabb2c8787a3a6ae57f9f58982
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-02-11 10:08:22 -0500

    Ensure taxonomy records are only moved and not copied.

--- otc/taxonomy/taxonomy.h
+++ otc/taxonomy/taxonomy.h
@@ -42,7 +42,10 @@ namespace otc
         std::bitset<32> flags;
         int depth = 0;
         int out_degree = 0;
+	taxonomy_record& operator=(taxonomy_record&& tr) = default;
+	taxonomy_record& operator=(const taxonomy_record& tr) = delete;
         taxonomy_record(taxonomy_record&& tr) = default;
+        taxonomy_record(const taxonomy_record& tr) = delete;
         explicit taxonomy_record(const std::string& line);
     };
 
