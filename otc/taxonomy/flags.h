28e8a7e40e438c6501f048b8c8f5ed78a7148f0a
Mark T. Holder <mtholder@gmail.com>
2017-03-02 11:38:35 -0600

    minor formatting for consistency with other files

--- otc/taxonomy/flags.h
+++ otc/taxonomy/flags.h
@@ -5,17 +5,18 @@
 #include <string>
 #include <vector>
 
-namespace otc
-{
-    typedef std::bitset<32> tax_flags;
-    int flag_from_string(const char* start, const char* end);
-    int flag_from_string(const std::string&);
-    tax_flags flags_from_string(const char* start, const char* end);
-    tax_flags flags_from_string(const std::string&);
-    std::string flag_to_string(int);
-    std::string flags_to_string(const tax_flags);
-    std::vector<std::string> flags_to_string_vec(const tax_flags flags);
-    std::bitset<32> cleaning_flags_from_config_file(const std::string& filename);
-}
+namespace otc {
+
+typedef std::bitset<32> tax_flags;
+int flag_from_string(const char* start, const char* end);
+int flag_from_string(const std::string&);
+tax_flags flags_from_string(const char* start, const char* end);
+tax_flags flags_from_string(const std::string&);
+std::string flag_to_string(int);
+std::string flags_to_string(const tax_flags);
+std::vector<std::string> flags_to_string_vec(const tax_flags flags);
+std::bitset<32> cleaning_flags_from_config_file(const std::string& filename);
+
+} //namespace otc
 
 #endif
