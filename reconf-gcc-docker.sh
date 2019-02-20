0e47c58b63ef4a90e183ca65135739bc8b391710
Benjamin Redelings <benjamin.redelings@gmail.com>
2019-02-20 16:37:16 -0500

    Remove autoconf files.

--- reconf-gcc-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CXX=$(which g++-5) CC=$(which gcc-5) \
-    CXXFLAGS="-Waddress -Warray-bounds -Wc++11-compat -Wchar-subscripts -Wcomment -Wformat -Wmain -Wmaybe-uninitialized -Wmissing-braces -Wnonnull -Wopenmp-simd -Wparentheses -Wreorder -Wreturn-type -Wsequence-point -Wsign-compare -Wstrict-aliasing -Wstrict-overflow -Wswitch -Wtrigraphs -Wuninitialized -Wno-unknown-pragmas -Wunused-function -Wunused-label -Wunused-value -Wunused-variable -Wvolatile-register-var -Wno-pragmas -pedantic -g -O0 -std=c++14" \
-    ../configure --with-webservices=yes --prefix=/otc
