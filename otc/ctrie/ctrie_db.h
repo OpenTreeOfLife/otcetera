5c8b6290b75cd160de8ccfbced75983ba1cc2f56
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-04-29 08:00:03 -0700

    Use CTrie2_t for the wide_trie as well.

--- otc/ctrie/ctrie_db.h
+++ otc/ctrie/ctrie_db.h
@@ -8,14 +8,13 @@ namespace otc {
 using CTrie3_t = CompressedTrie<CTrie3Node>;
 using CTrie2_t = CompressedTrie<CTrie2Node>;
 
-
 class CompressedTrieBasedDB {
     public:
     void initialize(const std::set<std::string> & keys);
     std::set<FuzzyQueryResult, SortQueryResByNearness>  fuzzy_query(const std::string & query_str) const;
     std::set<FuzzyQueryResult, SortQueryResByNearness>  exact_query(const std::string & query_str) const;
     private:
-    CTrie3_t wide_trie;
+    CTrie2_t wide_trie;
     CTrie2_t thin_trie;
 };
 
