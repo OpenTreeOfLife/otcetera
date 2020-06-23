b6ac322f35c1da350e71c194d7dca0272651bb9c
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-06-23 12:18:34 -0700

    Fix ctrie bug with SPACE.
    Avoid undefined shift by 64 bits.

--- otc/ctrie/ctrie_node.cpp
+++ otc/ctrie/ctrie_node.cpp
@@ -14,10 +14,10 @@ optional<std::size_t> CTrieNode::child_index_for_letter(stored_index_t letter) c
     auto lbits = get_letter_bits();
 
     // We don't have a bit for that letter.
-    if ((lbits & (ONE_64<<(63-letter))) == 0) return {};
+    if ((lbits & (HIGHEST_BIT>>letter)) == 0) return {};
 
     // remove bits for previous letters
-    uint64_t mask = ((uint64_t)(-1))<<(64-letter);
+    uint64_t mask = ((-1UL)<<(63-letter))<<1;
 
     // The number of letters BEFORE this letter
     int delta = __builtin_popcountl(mask & lbits);
