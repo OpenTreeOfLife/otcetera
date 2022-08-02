7111a91d7059b70ef6dce2aa4993d543be9b4b40
Benjamin Redelings <benjamin.redelings@gmail.com>
2022-08-02 16:36:58 -0400

    Factor out rollback info into solver/rollback.h

--- tools/solver/build.cpp
+++ tools/solver/build.cpp
@@ -1,4 +1,5 @@
 #include "build.h"
+#include "rollback.h"
 
 using std::vector;
 using std::list;
@@ -17,113 +18,6 @@ bool sort_cmp(const vector<T>& v1,  const vector<T>& v2)
 
     return (w1 == w2);
 }
-
-struct MergeRollbackInfo
-{
-    component_ref c1;
-    component_ref c2;
-    list<int>::const_iterator it;
-
-    shared_ptr<Solution> old_solution;
-
-    void unmerge(Solution& S);
-};
-
-void MergeRollbackInfo::unmerge(Solution& S)
-{
-    // Undo merging two components.
-    if (c2)
-    {
-        c2->elements.splice(c2->elements.end(), c1->elements, it, c1->elements.end());
-        for(auto& x: c2->elements)
-            S.component_for_index[x] = c2;
-    }
-    // Undo merge with trivial
-    else
-    {
-        S.component_for_index[c1->elements.back()] = nullptr;
-        c1->elements.pop_back();
-    }
-
-    if (old_solution)
-        c1->solution = old_solution;
-
-    c1->old_solutions.clear();
-    assert(c1->new_splits.empty());
-}
-
-struct SolutionRollbackInfo
-{
-    shared_ptr<Solution> S;
-    int n_old_implied_splits;
-    vector<MergeRollbackInfo> merge_rollback_info;
-    optional<int> n_orig_components;
-    optional<vector< shared_ptr<component_t> >> old_components;
-
-    void rollback();
-
-    explicit SolutionRollbackInfo(const shared_ptr<Solution>& s)
-        : S(s), n_old_implied_splits( s->implied_splits.size() )
-    {
-    }
-};
-
-void SolutionRollbackInfo::rollback()
-{
-    assert(n_old_implied_splits <= S->implied_splits.size());
-    S->implied_splits.resize(n_old_implied_splits);
-
-    // If n_orig_component is set, then we didn't quit early before the Merge( ) step.
-    if (n_orig_components and *n_orig_components == 0)
-    {
-        S->components.clear();
-
-        // If we are just going to _delete_ S later on, then this is a waste of time.
-        for(auto& c: S->component_for_index)
-            c = nullptr;
-
-        return;
-    }
-    else
-        for(int i=(int)merge_rollback_info.size()-1; i >= 0; i--)
-            merge_rollback_info[i].unmerge(*S);
-
-    // NOTE: Some components are created during merging that are
-    // (i) are not original components, and also
-    // (ii) end up being empty.  So they are not final components.
-
-    // * We need these components to survive (i.e not be destructed)
-    //   so that we can temporarily add elements to them during rollback.
-
-    // * We will then move these elements OUT of them into original
-    //   components.
-
-    // * These temporary components should all end up at the end of the
-    //   original components vector<> (the non-packed vector<>) because they
-    //   were added during merging by push_back( ).
-
-    // * We record the original size of the components vector BEFORE merging
-    //   and truncate to that length.  But before we truncate, we check that
-    //   rolling back merges leaves all the dropped components with no elements.
-    if (old_components)
-    {
-        assert(n_orig_components);
-        std::swap(S->components, *old_components);
-
-        for(int i=0;i<S->components.size();i++)
-            assert(S->components[i]);
-
-        for(int i=*n_orig_components;i<S->components.size();i++)
-            assert(S->components[i]->elements.empty());
-
-        assert(*n_orig_components <= S->components.size());
-        S->components.resize(*n_orig_components);
-
-        for(int i=0;i<S->components.size();i++)
-            assert(not S->components[i]->elements.empty());
-    }
-}
-
 
 void Solution::initialize_taxon_index_map() const
 {
