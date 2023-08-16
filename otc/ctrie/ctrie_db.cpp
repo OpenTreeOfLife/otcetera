0adaec55806d8208127712e1613a098ea43f8ac3
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-08-16 15:59:25 -0700

    Allow putting a few new keys into a different map.

--- otc/ctrie/ctrie_db.cpp
+++ otc/ctrie/ctrie_db.cpp
@@ -27,6 +27,10 @@ std::set<FuzzyQueryResult, SortQueryResByNearness> CompressedTrieBasedDB::fuzzy_
 
     auto from_full = wide_trie.fuzzy_matches(conv_query, max_dist);
     sorted.insert(std::begin(from_full), std::end(from_full));
+
+    auto from_new = new_trie->fuzzy_matches(conv_query, max_dist);
+    sorted.insert(std::begin(from_new), std::end(from_new));
+
     return sorted;
 }
 
@@ -42,6 +46,9 @@ std::set<FuzzyQueryResult, SortQueryResByNearness> CompressedTrieBasedDB::exact_
     auto from_full = wide_trie.fuzzy_matches(conv_query, 0);
     sorted.insert(std::begin(from_full), std::end(from_full));
 
+    auto from_new = new_trie->fuzzy_matches(conv_query, 0);
+    sorted.insert(std::begin(from_new), std::end(from_new));
+
     return sorted;
 }
 
@@ -54,10 +61,84 @@ vector<string> CompressedTrieBasedDB::prefix_query(const std::string & query_str
     auto from_full = wide_trie.prefix_query(conv_query);
     sorted.insert(sorted.end(), std::begin(from_full), std::end(from_full));
 
+    auto from_new = new_trie->prefix_query(conv_query);
+    sorted.insert(sorted.end(), std::begin(from_new), std::end(from_new));
+
     // I'm not sure this is a good idea...
     std::sort(sorted.begin(), sorted.end());
 
     return sorted;
+}
+
+void CompressedTrieBasedDB::add_key(const std::string& s)
+{
+    new_keys.insert(s);
+    rebuild_new_trie();
+}
+
+void CompressedTrieBasedDB::rebuild_new_trie()
+{
+    LOG(INFO)<<"Rebuilding ctree for additions";
+    LOG(INFO)<< new_keys.size() << " keys\n";
+
+    ctrie_init_set_t for_new;
+    std::map<stored_char_t, unsigned int> letter_counts;
+    std::set<stored_char_t> new_letter_set;
+    // unsigned mem_str = 0;
+    for (auto i : new_keys)
+    {
+        // mem_str += i.length();
+        auto widestr = to_u32string(i);
+
+	for_new.insert(widestr);
+
+	for (auto letter : widestr) {
+	    new_letter_set.insert(letter);
+	}
+        //LOG(INFO) << glob_conv8.to_bytes(widestr) << '\n';
+    }
+    LOG(INFO)<<for_new.size()<<" keys for new ctrie\n";
+
+    stored_str_t new_letters;
+    new_letters.insert(std::begin(new_letters), std::begin(new_letter_set), std::end(new_letter_set));
+    std::map<unsigned int, stored_str_t,std::greater<int>> by_count;
+
+    LOG(INFO)<<"new letters: "<<new_letters.size()<<"\n";
+    for (auto letter : new_letters)
+        LOG(INFO)<<"  "<<to_char_str(letter)<<"\n";
+    LOG(INFO)<<"done\n";
+
+    for (auto [letter,count] : letter_counts)
+        by_count[count].push_back(letter);
+
+    int n_dropped_letters = 0;
+    for(auto [count,letters]: by_count)
+    {
+        LOG(INFO)<<"  "<<count<<" : \n";
+        for(auto letter : letters)
+        {
+            LOG(INFO)<<"    "<<to_char_str(letter)<<" ("<<letter<<")";
+            if (new_letters.size() < 64)
+                new_letters.push_back(letter);
+            else
+            {
+                n_dropped_letters++;
+                LOG(INFO)<<"  DROPPED!";
+            }
+            LOG(INFO)<<"\n";
+        }
+    }
+    LOG(INFO)<<"done\n";
+
+    if (n_dropped_letters)
+    {
+        LOG(INFO)<<"dropped "<<n_dropped_letters<<" letters.\n";
+    }
+
+    //LOG(INFO) << "set size = " << (sizeof(std::string *) + sizeof(char *) + 8)*keys.size() + mem_str << "bytes\n";
+
+    new_trie = std::make_shared<CompressedTrie>();
+    new_trie->init(for_new, new_letters);
 }
 
 void CompressedTrieBasedDB::initialize(const std::set<std::string> & keys) {
@@ -137,6 +218,9 @@ void CompressedTrieBasedDB::initialize(const std::set<std::string> & keys) {
     //std::cerr << "set size = " << (sizeof(std::string *) + sizeof(char *) + 8)*keys.size() + mem_str << "bytes\n";
     wide_trie.init(for_wide, wide_letters);
     thin_trie.init(for_thin, thin_letters);
+
+    new_trie = std::make_shared<CompressedTrie>();
+    new_trie->init({},thin_letters);
 
     /*
     wide_trie.db_write_words(std::cerr);
