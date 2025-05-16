630e56fee28014e236179a7d43d369a6299c685e
Mark T. Holder <mtholder@gmail.com>
2025-05-16 16:55:39 -0500

    minor

--- scripts/semanticize_amendments.py
+++ scripts/semanticize_amendments.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """ConstGitRepo is a base class containing methods that specify
 repo-specific information, but no methods that alter the state
